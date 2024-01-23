766b605890dfaad405eb5a40fb81615de3e41feb
David Yoon <davidyoon@hanwha.com>
2024-01-23 13:57:57 +0900

    Initial Commit

---
+++ MVVM-C/MVVM_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_CApp.swift
+//  MVVM-C
+//
+//  Created by Davidyoon on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
